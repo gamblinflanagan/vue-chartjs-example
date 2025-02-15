4e84d283b12ec9d89ed85cd42bae4f6a454c8fe4
gamblinflanagan <gamblinflanagan97@gmail.com>
2025-02-15 17:40:18 -0500

    add line chart

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -8,22 +8,84 @@ defineProps({
 </script>
 
 <script>
-import { Bar } from 'vue-chartjs'
 import {
   Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+// import { Bar } from 'vue-chartjs'
+// import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(
   Title,
   Tooltip,
   Legend,
   BarElement,
+  LineElement,
   CategoryScale,
   LinearScale,
-} from 'chart.js'
+  PointElement,
+)
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
+// export default {
+//   name: 'BarChart',
+//   components: { Bar },
+//   data() {
+//     return {
+//       chartData: {
+//         labels: [
+//           'January',
+//           'February',
+//           'March',
+//           'April',
+//           'May',
+//           'June',
+//           'July',
+//           'August',
+//           'September',
+//           'October',
+//           'November',
+//           'December',
+//         ],
+//         datasets: [
+//           {
+//             label: 'Bar Dataset',
+//             data: [65, 59, 80, 81, 56, 55, 40],
+//             backgroundColor: [
+//               'rgba(255, 99, 132, 0.2)',
+//               'rgba(255, 159, 64, 0.2)',
+//               'rgba(255, 205, 86, 0.2)',
+//               'rgba(75, 192, 192, 0.2)',
+//               'rgba(54, 162, 235, 0.2)',
+//               'rgba(153, 102, 255, 0.2)',
+//               'rgba(201, 203, 207, 0.2)',
+//             ],
+//             borderColor: [
+//               'rgb(255, 99, 132)',
+//               'rgb(255, 159, 64)',
+//               'rgb(255, 205, 86)',
+//               'rgb(75, 192, 192)',
+//               'rgb(54, 162, 235)',
+//               'rgb(153, 102, 255)',
+//               'rgb(201, 203, 207)',
+//             ],
+//             borderWidth: 1,
+//           },
+//         ],
+//       },
+//     }
+//   },
+// }
 export default {
-  name: 'BarChart',
-  components: { Bar },
+  name: 'LineChart',
+  components: { Line },
   data() {
     return {
       chartData: {
@@ -43,27 +105,11 @@ export default {
         ],
         datasets: [
           {
-            label: 'Bar Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 205, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(201, 203, 207, 0.2)',
-            ],
-            borderColor: [
-              'rgb(255, 99, 132)',
-              'rgb(255, 159, 64)',
-              'rgb(255, 205, 86)',
-              'rgb(75, 192, 192)',
-              'rgb(54, 162, 235)',
-              'rgb(153, 102, 255)',
-              'rgb(201, 203, 207)',
-            ],
-            borderWidth: 1,
+            label: 'Line Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40, 45, 69, 88, 80, 95],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1,
           },
         ],
       },
@@ -73,10 +119,10 @@ export default {
 </script>
 
 <template>
-  <h1>this is the bar chart: {{ name }}</h1>
+  <!-- <h1>this is the bar chart: {{ name }}</h1> -->
   <br />
   <div>
-    <Bar :data="chartData" :options="chartOptions" />
+    <Line :data="chartData" :options="chartOptions" />
   </div>
 </template>
 
