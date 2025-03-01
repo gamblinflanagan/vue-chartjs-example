5da102753013a0291404e5418b8ef12c6063bdae
gamblinflanagan <gamblinflanagan97@gmail.com>
2025-03-01 12:34:14 -0500

    Add url prop, ammend styles, make charts the index page

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -3,20 +3,9 @@
 </script>
 
 <template>
-  <!-- <div class="about"> -->
-  <div>
-    <!-- <h1>This is the page to put the charts</h1> -->
-    <!-- <BarChart v-for="i in 5" num1="5" /> -->
-    <LineChart chartName="Global M2" />
-  </div>
+  <LineChart chartName="Global M2" chartUrl="https://fed-charts-default-rtdb.firebaseio.com/observations.json" />
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
